bb68d28094ea2fa5eda85b2f225a6503fb949c20
hamd36 <mohamed1999250@gmail.com>
2021-10-16 18:24:11 +0200

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,58 +4,58 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
 
-    <div class="d-flex justify-content-center mt-7 h-100">
-        <div class="user_card">
-            <div class="d-flex justify-content-center">
-                <div class="brand_logo_container">
-                    <img src="{{asset('imgs/logo.jpg')}}" class="brand_logo" alt="Logo">
-                </div>
-            </div>
-            <div class="d-flex justify-content-center form_container">
-                <form method="POST" action="{{ route('login') }}">
+<section>
+    <div class="page-header min-vh-75">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+            <div class="card card-plain mt-8">
+              <div class="card-header pb-0 text-left bg-transparent">
+                <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
+                <p class="mb-0">Enter your email and password to sign in</p>
+              </div>
+              <div class="card-body">
+                <form method="POST" action="{{ route('login') }}" role="form">
                     @csrf
-                    <div class="input-group mb-3">
-                        <div class="input-group-append">
-                            <span class="input-group-text"><i class="bi bi-person-bounding-box"></i></span>
-                        </div>
-                        <input type="email" name="email" class="form-control input_user @error('email') is-invalid @enderror" value="" placeholder="username">
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                    <div class="input-group mb-2">
-                        <div class="input-group-append">
-                            <span class="input-group-text"><i class="bi bi-key"></i></span>
-                        </div>
-                        <input type="password" name="password" class="form-control input_pass @error('password') is-invalid @enderror" value="" placeholder="password">
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div>
-                    </div>
-                        <div class="d-flex justify-content-center mt-3 login_container">
-                 <button type="submit" name="button" class="btn login_btn">Login</button>
-               </div>
+                  <label>Email</label>
+                  <div class="mb-3">
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                  <label>Password</label>
+                  <div class="mb-3">
+                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                 </div>
+                  <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="remember">Remember me</label>
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+                  </div>
                 </form>
+              </div>
+ 
             </div>
- 
+          </div>
+          <div class="col-md-6">
+            <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+              <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../imgs/cover.jpg')"></div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </section>
 
- 
-</div>
 @endsection
