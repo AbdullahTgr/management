4a23442ec5ca241cb6e7e8a10dc3642daccbdb3f
AbdullahTgr <4multijob@gmail.com>
2021-10-17 11:35:14 +0200

    test

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,7 +26,7 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Dashboard</span>
+          <span class="nav-link-text ms-1" style="color: #82d616;">Dashboard</span>
         </a>
       </li>
       <li class="nav-item">
