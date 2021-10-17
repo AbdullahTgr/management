9c6bde18694fc9b517ecca8ff82d63f7a19b1a5b
AbdullahTgr <4multijob@gmail.com>
2021-10-17 18:02:52 +0200

    sdfsdf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -196,9 +196,6 @@
               <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                   <li class="nav-item">
-                    <a href="#" class="nav-link text-muted" target="_blank">Creative Tim</a>
-                  </li>
-                  <li class="nav-item">
                     <a href="#" class="nav-link text-muted" target="_blank">About Us</a>
                   </li>
                   <li class="nav-item">
