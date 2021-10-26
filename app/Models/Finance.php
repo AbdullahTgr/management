030abf9275fcbadf39784b964ffdd1ef0b7f3594
mohamed mostafa <mohamed1999250@gmail.com>
2021-10-26 18:09:45 +0200

    fix

--- app/Models/Finance.php
+++ app/Models/Finance.php
@@ -32,7 +32,7 @@ class Finance extends Model
 
     public function res() 
     {
-        return $this->belongsTo(Reservation::class, 'res_id');
+        return $this->belongsTo(Reservation::class, $this->res_id);
     }
     
     public function banks()
